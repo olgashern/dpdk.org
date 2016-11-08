d078ae6e507b94ad5d0eeb45e724ee6b7cfc8cf6
Nelio Laranjeiro <nelio.laranjeiro@6wind.com>
2016-11-08 10:56:39 +0100

    net: introduce big and little endian types
    This commit introduces new rte_{le,be}{16,32,64}_t types and updates
    rte_{le,be,cpu}_to_{le,be,cpu}_*() and network header structures
    accordingly.
    
    Specific big/little endian types avoid uncertainty and conversion mistakes.
    
    No ABI change since these are simply typedefs to the original types.
    
    Signed-off-by: Nelio Laranjeiro <nelio.laranjeiro@6wind.com>

--- lib/librte_net/rte_gre.h
+++ lib/librte_net/rte_gre.h
@@ -45,23 +45,23 @@ extern "C" {
  */
 struct gre_hdr {
 #if RTE_BYTE_ORDER == RTE_LITTLE_ENDIAN
-	uint16_t res2:4; /**< Reserved */
-	uint16_t s:1;    /**< Sequence Number Present bit */
-	uint16_t k:1;    /**< Key Present bit */
-	uint16_t res1:1; /**< Reserved */
-	uint16_t c:1;    /**< Checksum Present bit */
-	uint16_t ver:3;  /**< Version Number */
-	uint16_t res3:5; /**< Reserved */
+	uint16_t res2:4;  /**< Reserved */
+	uint16_t s:1;     /**< Sequence Number Present bit */
+	uint16_t k:1;     /**< Key Present bit */
+	uint16_t res1:1;  /**< Reserved */
+	uint16_t c:1;     /**< Checksum Present bit */
+	uint16_t ver:3;   /**< Version Number */
+	uint16_t res3:5;  /**< Reserved */
 #elif RTE_BYTE_ORDER == RTE_BIG_ENDIAN
-	uint16_t c:1;    /**< Checksum Present bit */
-	uint16_t res1:1; /**< Reserved */
-	uint16_t k:1;    /**< Key Present bit */
-	uint16_t s:1;    /**< Sequence Number Present bit */
-	uint16_t res2:4; /**< Reserved */
-	uint16_t res3:5; /**< Reserved */
-	uint16_t ver:3;  /**< Version Number */
+	uint16_t c:1;     /**< Checksum Present bit */
+	uint16_t res1:1;  /**< Reserved */
+	uint16_t k:1;     /**< Key Present bit */
+	uint16_t s:1;     /**< Sequence Number Present bit */
+	uint16_t res2:4;  /**< Reserved */
+	uint16_t res3:5;  /**< Reserved */
+	uint16_t ver:3;   /**< Version Number */
 #endif
-	uint16_t proto;  /**< Protocol Type */
+	rte_be16_t proto; /**< Protocol Type */
 } __attribute__((__packed__));
 
 #ifdef __cplusplus
